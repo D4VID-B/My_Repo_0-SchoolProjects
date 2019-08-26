c7cc2da46424cc0f130fbf3e3254e62fcb3de3b3
David Bakaleinik <david.bakaleinik@mymail.champlain.edu>
2019-08-26 14:00:13 -0400

    Adding new project from Game Tech II

---
+++ Test_Project/Assets/Respawn_Script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn_Script : MonoBehaviour
+{
+    Vector3 spawn;
+    void OnTriggerEnter()
+    {
+
+    }
+}

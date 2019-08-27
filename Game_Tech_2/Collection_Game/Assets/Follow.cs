d44a7a4973a598e1cb5999d18eb542bc14ebb679
David Bakaleinik <david.bakaleinik@mymail.champlain.edu>
2019-08-27 16:15:48 -0400

    Working on Collection Game

---
+++ Game_Tech_2/Collection_Game/Assets/Follow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform target;
+    public float height = 5f;
+    public float distance = 10f;
+    public float rotDamping = 5f;
+
+    
+
+    void LateUpdate()
+    {
+        float wantedRotationAngle = target.eulerAngles.y;
+        float currentRotationAngle = transform.eulerAngles.y;
+        float wantedHeight = target.position.y + height;
+        float currentHeight = transform.position.y;
+
+        //LERP the rotation
+        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotDamping * Time.deltaTime);
+        //LERP the height
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, Time.deltaTime);
+        //Get the rotation
+        Quaternion currentRotation = Quaternion.Euler(0f, currentRotationAngle, 0f);
+        //position the camera
+        transform.position = target.transform.position;
+        //set its offset distance
+        transform.position -= currentRotation * Vector3.forward * distance;
+        //set its offset height
+        Vector3 newHeight = transform.position;
+        newHeight.y = currentHeight;
+        transform.position = newHeight;
+        transform.LookAt(target);
+
+    }
+}
---
+++ Game_Tech_2/Collection_Game/Assets/PickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().ChangeScore();
+
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}

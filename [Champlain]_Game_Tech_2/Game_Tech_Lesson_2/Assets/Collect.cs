85fce31661d5898548f751a276cf6df081ad3472
David <davidbakaleynik1998@gmail.com>
2019-09-02 20:56:51 +0200

    Adding Lesson 2 Project

---
+++ [Champlain]_Game_Tech_2/Game_Tech_Lesson_2/Assets/Collect.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ [Champlain]_Game_Tech_2/Game_Tech_Lesson_2/Assets/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float xRot = 3f;
+    public float yRot = 3f;
+    public float zRot = 3f;
+
+    void Update()
+    {
+        transform.Rotate(xRot, yRot, zRot);
+    }
+}

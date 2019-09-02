872a89d7ce63cd08a12a2aad7700601fddd6ce53
David <davidbakaleynik1998@gmail.com>
2019-09-02 18:25:50 +0200

    Organising Repository and updating a corrupted project

---
+++ [Champlain]_Game_Physics/GP_Intro/Assets/Scripts/Bounce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float bounceStrength = 2;
+    public ForceMode mode;
+
+    Vector3 bounceVector = new Vector3();
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision!");
+
+        bounceVector.y = bounceStrength * 0.981f;
+
+        bounceStrength = bounceVector.y;
+
+        rb.AddForce(bounceVector, mode);
+
+        //Debug.Log("Bounce Strength: " + bounceVector.y.ToString());
+        
+    }
+}

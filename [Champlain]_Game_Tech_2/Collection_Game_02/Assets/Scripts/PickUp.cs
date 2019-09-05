79e542d63f8578769096483caf062b7e63783f7a
David <davidbakaleynik1998@gmail.com>
2019-09-05 22:28:26 +0200

    Transfering scripts from previous project

---
+++ [Champlain]_Game_Tech_2/Collection_Game_02/Assets/Scripts/PickUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    public ParticleSystem ps;
+
+    public enum OnPickup
+    {
+        Emit_Particles,
+        Rrmove_Object
+    }
+
+    public OnPickup PickupAction;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().ChangeScore();
+
+
+        if (PickupAction == OnPickup.Emit_Particles)
+        {
+            ps.Play();
+        }
+        else if(PickupAction == OnPickup.Rrmove_Object)
+        {
+            gameObject.SetActive(false);
+        }
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}

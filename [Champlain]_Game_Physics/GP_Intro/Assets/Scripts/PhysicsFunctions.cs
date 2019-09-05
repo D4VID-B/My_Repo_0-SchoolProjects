635c82315478e45a491a37372c0d4fae60f30d75
David <davidbakaleynik1998@gmail.com>
2019-09-05 21:52:22 +0200

    Working on Collection game and Physics

---
+++ [Champlain]_Game_Physics/GP_Intro/Assets/Scripts/PhysicsFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/***** Formulas + most of the code is taken from [Game Physics: Engine Development, by Ian Millington] *****/
+
+public class PhysicsFunctions : MonoBehaviour
+{
+    
+}
+
+class CustomVector3
+{
+    public float x, y, z;
+
+    private float pad;
+
+    void invert()
+    {
+        x = -x;
+        y = -y;
+        z = -z;
+    }
+}

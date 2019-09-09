6272864a810574aa45dde4a7b6a4ad1c67c5194a
David <davidbakaleynik1998@gmail.com>
2019-09-09 21:12:00 +0200

    Lesson 3

---
+++ [Champlain]_Game_Tech_2/Lesson03/Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    float cameraOffset;
+
+    void Start()
+    {
+        //cameraOffset = Camera.main.transform.position.z - GameManager.
+    }
+
+    void Update()
+    {
+        checkForClick();
+    }
+
+    void checkForClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            explode();
+        }
+    }
+
+    void explode()
+    {
+        GameObject explosion = Instantiate(explosionPrefab);
+
+        Vector3 position = Input.mousePosition;
+        position.z = 10f;
+
+        position = Camera.main.ScreenToWorldPoint(position);
+        explosion.transform.position = position;
+
+        Destroy(explosion, explosion.GetComponent<ParticleSystem>().main.duration);
+    }
+}

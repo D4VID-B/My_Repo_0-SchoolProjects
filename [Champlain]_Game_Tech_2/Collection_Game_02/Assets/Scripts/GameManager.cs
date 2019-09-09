f438f6fedaa9a7b28fbcff628727ebeb31db3300
David Bakaleinik <david.bakaleinik@mymail.champlain.edu>
2019-09-09 16:16:50 -0400

    Updating

--- [Champlain]_Game_Tech_2/Collection_Game_02/Assets/Scripts/GameManager.cs
+++ [Champlain]_Game_Tech_2/Collection_Game_02/Assets/Scripts/GameManager.cs
@@ -10,43 +10,44 @@ public class GameManager : MonoBehaviour
     public GameObject endPanel;
     public GameObject gate;
 
-    //public static GameManager instance;
+    public static GameManager instance;
+    public int score = 0;
 
     void Awake()
     {
         findObjects();
 
-        //if (instance == null)
-        //{
-        //    instance = this;
+        if (instance == null)
+        {
+            instance = this;
 
-        //    DontDestroyOnLoad(gameObject);
-        //}
-        //else if (instance != null)
-        //{
-        //    Destroy(gameObject);
-        //}
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void Start()
     {
         findObjects();
         endPanel.SetActive(false);
-        //GameObject.Find("EndPanel").SetActive(false);
+        
 
         Debug.Log(endPanel.ToString());
     }
 
     void Update()
     {
-        if (ScoreScript.instance.getScore() == 16 && SceneManager.GetActiveScene().name == "Level01")
+        if (score == 16 && SceneManager.GetActiveScene().name == "Level01")
         {
             endPanel.SetActive(true);     //Attempt 1
             //GameObject.Find("EndPanel").SetActive(true);        //Attempt 2
             gate.GetComponent<Gate>().openGate();
             //GameObject.Find("Gate").GetComponent<Gate>().openGate();
         }
-        else if (ScoreScript.instance.getScore() >= 32 && SceneManager.GetActiveScene().name == "Level02")
+        else if (score >= 32 && SceneManager.GetActiveScene().name == "Level02")
         {
             endPanel.SetActive(true);
             //GameObject.Find("EndPanel").SetActive(true);
@@ -58,9 +59,10 @@ public class GameManager : MonoBehaviour
 
     public void ChangeScore(int amount)
     {
-        ScoreScript.instance.setScore(amount);
+        //ScoreScript.instance.setScore(amount);
+        score += amount;
 
-        Debug.Log("Score: " + ScoreScript.instance.getScore().ToString());
+        Debug.Log("Score: " + score);
        
     }
 
@@ -69,7 +71,7 @@ public class GameManager : MonoBehaviour
         if (endPanel == null)
         {
             endPanel = GameObject.Find("EndPanel");
-            //endPanel = GameObject.Find("EndPanel02");
+            endPanel = GameObject.Find("EndPanel02");
 
             Debug.Log("Panel Found");
         }
@@ -77,7 +79,7 @@ public class GameManager : MonoBehaviour
         if (gate == null)
         {
             gate = GameObject.Find("Gate");
-            //gate = GameObject.Find("Gate02");
+            gate = GameObject.Find("Gate02");
 
             Debug.Log("Gate Found");
         }

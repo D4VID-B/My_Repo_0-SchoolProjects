a9cbdf8355111053409e5c59227d4f31aca88cfb
David Bakaleinik <david.bakaleinik@mymail.champlain.edu>
2019-10-26 11:36:22 -0400

    Creating and starting tools Project

---
+++ [Champlain]_Game_Tech_2/Assignment07-Tools/Assets/Scripts/Tools/Collector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// A tool for searching and collecting a set of user-determined objects as well as providing the user a number of functions base off of the object type.
+/// </summary>
+public class Collector : MonoBehaviour
+{
+    public enum ObjectClass
+    {
+
+    }
+
+    public ObjectClass ObjectType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ [Champlain]_Game_Tech_2/Assignment07-Tools/Assets/Scripts/Tools/SpawnAtPos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAtPos : MonoBehaviour
+{
+
+    public Vector3 custonSpawnPosition;
+    public Vector3 customSpawnRotation;
+    public Vector3 customSpawnScale;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

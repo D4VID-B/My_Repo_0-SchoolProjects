509c3cf3f457e4b40d485d87a5fc91f10c919974
David <davidbakaleynik1998@gmail.com>
2019-10-26 17:35:44 +0200

    Lesson 8

---
+++ [Champlain]_Game_Tech_2/GT_Lesson_08/Assets/Editor/SomeScriptEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SomeScript))]
+
+public class SomeScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.HelpBox("Here is some random info", MessageType.Info);
+        EditorGUILayout.HelpBox("Error: Everything is on fire.", MessageType.Error);
+        EditorGUILayout.HelpBox("Warning: Everything is about to be on fire.", MessageType.Warning);
+        EditorGUILayout.HelpBox("Stuff!", MessageType.None);
+        base.OnInspectorGUI();
+
+    }
+
+}
+
---
+++ [Champlain]_Game_Tech_2/GT_Lesson_08/Assets/SomeScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeScript : MonoBehaviour
+{
+    public int someInt;
+
+    public string someString;
+
+    public Transform someTransform;
+
+    public Color someColor;
+
+}

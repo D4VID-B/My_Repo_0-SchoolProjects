7b335098cfd69d0676fb1a2e6f0f69a3082451e4
David <davidbakaleynik1998@gmail.com>
2019-10-28 19:54:46 +0100

    Lesson 9

---
+++ [Champlain]_Game_Tech_2/GT_Lesson_09/Assets/Editor/EnemyEditor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EnemyScript))]
+[CanEditMultipleObjects]
+public class EnemyEditor : Editor
+{
+
+    SerializedProperty damageProp;
+    SerializedProperty armorProp;
+    SerializedProperty weaponProp;
+
+    private void OnEnable()
+    {
+        damageProp = serializedObject.FindProperty("damage");
+        armorProp = serializedObject.FindProperty("armor");
+        weaponProp = serializedObject.FindProperty("weapon");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EnemyScript instance = (EnemyScript)target;
+
+        //instance.damage = EditorGUILayout.IntSlider("Damange", instance.damage, 0, 100);
+        EditorGUILayout.IntSlider(damageProp, 0, 100, new GUIContent("damage"));
+
+        if (!damageProp.hasMultipleDifferentValues)
+            progressBar(damageProp.intValue /100f, "Damage");
+
+        //instance.armor = EditorGUILayout.IntSlider("Armor", instance.armor, 0, 100);
+        EditorGUILayout.IntSlider(armorProp, 0, 100, new GUIContent("Armor"));
+
+        if (!armorProp.hasMultipleDifferentValues)
+            progressBar(armorProp.intValue / 100f, "Armor");
+
+        //bool allowSceneObject = !EditorUtility.IsPersistent(target);
+        //instance.weapon = (GameObject)EditorGUILayout.ObjectField("Weapon", instance.weapon, typeof(GameObject), allowSceneObject);
+
+        EditorGUILayout.PropertyField(weaponProp, new GUIContent("Weapon"));
+
+        serializedObject.ApplyModifiedProperties();
+        
+        //base.OnInspectorGUI();
+    }
+
+    void progressBar(float value, string label)
+    {
+        Rect rect = GUILayoutUtility.GetRect(18f, 18f, "TextField");
+
+        EditorGUI.ProgressBar(rect, value, label);
+        EditorGUILayout.Space();
+    }
+}
---
+++ [Champlain]_Game_Tech_2/GT_Lesson_09/Assets/Editor/SampleWindow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SampleWindow : EditorWindow
+{
+    string aString;
+    bool aBool = true;
+    float aFloat = 1.1f;
+
+    bool groupEnabled;
+
+    [MenuItem("Window/Sample Window")]
+ static void showWindow()
+    {
+        EditorWindow.GetWindow(typeof(SampleWindow));
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Sample Winow", EditorStyles.boldLabel);
+        aString = EditorGUILayout.TextField("A String", aString);
+
+
+        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+       
+            aBool = EditorGUILayout.Toggle("A Boolean", aBool);
+
+            aFloat = EditorGUILayout.Slider("A Slider", aFloat, -100f, 100f);
+        
+        EditorGUILayout.EndToggleGroup();
+        
+    }
+
+}

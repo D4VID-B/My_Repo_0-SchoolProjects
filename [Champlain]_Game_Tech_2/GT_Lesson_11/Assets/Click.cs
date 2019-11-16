601781f43d7bd556d8e024775ce89140757fda3b
David <davidbakaleynik1998@gmail.com>
2019-11-16 17:06:45 +0100

    Lesson 11

---
+++ [Champlain]_Game_Tech_2/GT_Lesson_11/Assets/Click.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    void OnMouseUp()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}

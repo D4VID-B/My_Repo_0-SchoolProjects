938763551d8d3b492a96ea525489a74b1ad67250
David <davidbakaleynik1998@gmail.com>
2019-11-30 17:04:59 +0100

    Adding Lesson 13

---
+++ [Champlain]_Game_Tech_2/GT_Lesson_13/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float forward;
+    float rot;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forward = Input.GetAxis("Vertical");
+        rot = Input.GetAxis("Horizontal");
+
+        rb.AddForce(transform.forward * forward * 20f);
+
+        transform.Rotate(0f, rot * 5f, 0f);
+    }
+}
